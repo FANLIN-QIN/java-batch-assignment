ace62d7f23b0b1b435020bcda1b2987e268985ee
FANLIN-QIN <70307077+FANLIN-QIN@users.noreply.github.com>
2022-02-07 01:57:13 -0800

    Add files via upload

---
+++ JAVA/Ass2/Department.java
@@ -0,0 +1,48 @@
+package hw2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "course")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Student_Department> student_department= new ArrayList<>();
+
+    public List<Student_Department> getStudent_Department() {
+        return student_department;
+    }
+
+    public void setStudent_Department(List<Student_Department> student_department) {
+        this.student_department = student_department;
+    }
+
+    public void add_Student_Department(Student_Department sd) {
+        this.student_department.add(sd);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
