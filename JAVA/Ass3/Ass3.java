11a8eb392d18bcf956ea4e06591ba8f14fdc9265
FANLIN-QIN <70307077+FANLIN-QIN@users.noreply.github.com>
2022-02-11 02:30:58 -0800

    Add files via upload

---
+++ JAVA/Ass3/Ass3.java
@@ -0,0 +1,11 @@
+package RESTdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ass3 {
+    public static void main(String[] args) {
+        SpringApplication.run(Ass3.class, args);
+    }
+}
